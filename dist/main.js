fa981b80ac60bac031e87a64f1b3588b53194cfb
David Fraas <david-fraas@web.de>
2021-01-04 18:37:20 +0100

    nav trigger update

--- dist/main.js
+++ dist/main.js
@@ -12,6 +12,16 @@ var n = d.getFullYear();
 
 $("#footeryear").text(n);
 
+$(document).on("click", function(e)
+{
+    var container = $(".nav-trigger");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0)
+    {
+        $('.navbar').collapse("toggle");
+    }
+});
 
 $(".nav-trigger").on("click", function(){
   $('.navbar').collapse("toggle");
